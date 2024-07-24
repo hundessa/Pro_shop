dc0480709908e52ee3dc8de66a9866852a7dc1ff
hundessa <hundserb@gmail.com>
2024-07-24 10:51:48 +0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./components/screens/HomeScreen";
+
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+    <HomeScreen/>
+      </Container>
+    </main>
+    <Footer />
+    </>
+  )
+}
+
+export default App
